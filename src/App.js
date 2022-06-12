88411ee637577cbb437d0aa5876a2c441e104e22
Christian Seyoum <chriscoding96@gmail.com>
2022-06-12 13:34:29 -0400

    Added the rest of the dates

--- src/App.js
+++ src/App.js
@@ -9,21 +9,35 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className='App-header'>
+      <div className='App-Outer'>
           {open ? <Notes date={position} setOpen={setOpen}/> : 
+        <div className='App-Outer'>
           <div className='App-header'>
             {Array.from({ length: 7 }).map((_, i) => (
               <Date key={i} Date={i + 1} setOpen={setOpen} setPosition={setPosition}/>
             ))}
-            {/* <br/>
+          </div>
+          <div className='App-header'>
             {Array.from({ length: 7 }).map((_, i) => (
               <Date key={i} Date={i + 8} setOpen={setOpen} setPosition={setPosition}/>
             ))}
-            <br/>
+          </div>
+          <div className='App-header'>
             {Array.from({ length: 7 }).map((_, i) => (
               <Date key={i} Date={i + 15} setOpen={setOpen} setPosition={setPosition}/>
-            ))} */}
+            ))}
           </div>
+          <div className='App-header'>
+            {Array.from({ length: 7 }).map((_, i) => (
+              <Date key={i} Date={i + 22} setOpen={setOpen} setPosition={setPosition}/>
+            ))}
+          </div>
+          <div className='App-header'>
+            {Array.from({ length: 3 }).map((_, i) => (
+              <Date key={i} Date={i + 29} setOpen={setOpen} setPosition={setPosition}/>
+            ))}
+          </div>
+        </div>
           }
       </div>
       
